842167b993de03b471e939d8822e105d2ab0d22c
cstanley2 <61754447+cstanley2@users.noreply.github.com>
2020-05-07 18:20:13 -0700

    Room class with tester
    Added room class to TriviaMaze

---
+++ TriviaMaze/Tester.java
@@ -0,0 +1,13 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Room room = new Room (0, 0);
+		Room room2 = new Room(1, 1);
+		Room room3 = new Room(3, 3);
+		System.out.println(room.toString() + room2.toString() + room3.toString());
+		
+	}
+
+}
