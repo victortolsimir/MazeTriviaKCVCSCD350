8bd0078da0459c740531de124c71f3f4fd19135f
victortolsimir <57809817+victortolsimir@users.noreply.github.com>
2020-05-20 15:18:20 -0700

    Add files via upload
    Added tests for the implementation of the memento pattern

---
+++ TriviaMaze/src/tests/SavingStateTests.java
@@ -0,0 +1,27 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import triviaMaze.Maze;
+import triviaMaze.Player;
+import triviaMaze.Tester;
+
+class SavingStateTests {
+
+	@Test
+	void saveGameTest() {
+		
+		Player player = new Player(1,1);
+		Maze maze = new Maze(player);
+		
+		Tester.saveGame(maze);
+		
+		Maze savedGame = Tester.loadGame().getMaze();
+		
+		assertEquals(0, maze.toString().compareTo(savedGame.toString()));
+		//fail("Not yet implemented");
+	}
+
+}
